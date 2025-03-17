8677e20dc591e97970c3412254c4fd3e7980a059
Luca Sandrock <luca.sandrock@proton.me>
2025-03-17 14:19:08 +0100

    5 current   2025-03-17 14:19:06  25.05.20250315.c80f6a7     6.13.7                          *

--- modules/system/software_gui.nix
+++ modules/system/software_gui.nix
@@ -91,20 +91,7 @@
   ];
 
   programs = {
-    gamemode = {
-      enable = true;
-      enableRenice = true;
-      settings = {
-        general = {
-          softrealtime = "auto";
-          renice = 10;
-        };
-        custom = {
-          start = "notify-send -a 'Gamemode' 'Optimizations activated'";
-          end = "notify-send -a 'Gamemode' 'Optimizations deactivated'";
-        };
-      };
-    };
+    gamemode.enable = true;
     hyprland.enable = true;
     hyprlock.enable = true;
     spicetify =
