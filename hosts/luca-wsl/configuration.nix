6b5be95ea244f8eba062f8a44136d36624252015
Luca Sandrock <luca.sandrock@proton.me>
2025-03-20 20:01:21 +0100

    18 current  2025-03-20 19:28:03  25.05.20250318.b6eaf97     6.13.7                          *

--- hosts/luca-wsl/configuration.nix
+++ hosts/luca-wsl/configuration.nix
@@ -1,5 +1,13 @@
-{ ... }:
+inputs@{ ... }:
 {
   imports = [ ../../modules/system/profiles/headless.nix ];
   wsl.enable = true;
+
+  home-manager = {
+    backupFileExtension = "backup";
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "luca" = import ./home.nix;
+    };
+  };
 }
